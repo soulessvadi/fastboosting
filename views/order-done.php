bb0ce7642e370bc086946cef9d12157b28e677b3
soulessvadi <ondarkpath@gmail.com>
2018-04-28 14:40:37 +0300

    congrats

--- views/order-done.php
+++ views/order-done.php
@@ -63,29 +63,86 @@
 							<button class="btn btn-red runner">Оставить отзыв</button>
 						</div>
 					</div>
+
 					<div class="order-done--bonus">
 						<h6>Ваш бонус</h6>
 						<div class="text-center">
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita aliquam, voluptatibus placeat dolores maiores magni eveniet odit quaerat molestias, omnis, temporibus voluptatum. Culpa ratione, repellendus minus, accusamus excepturi laborum sint?</p>
 						</div>
 						<div class="bonus-selector flex">
-							<div class="choise column active">
-								<label>
+							<div class="choise column red">
+								<label class="choise-block">
+									<div class="choise-gift">
+										<span class="icon" style="background-image: url(public/img/icons/discountico.png)"></span>
+										<span class="name">Промокод на скидку</span>
+									</div>
 									<input type="radio" name="bonus">
 								</label>
 							</div>
 							<div class="choise column opened">
-								<label>
+								<label class="choise-block">
+									<div class="choise-gift">
+										<span class="icon" style="background-image: url(public/img/icons/pocketico.png)"></span>
+										<span class="name">Бонус на счет (руб)</span>
+									</div>
 									<input type="radio" name="bonus">
 								</label>
 							</div>
-							<div class="choise column">
-								<label>
+							<div class="choise column red opened">
+								<label class="choise-block">
+									<div class="choise-gift">
+										<span class="icon" style="background-image: url(public/img/icons/pacmanico.png)"></span>
+										<span class="name">скидка на аккаунт</span>
+									</div>
 									<input type="radio" name="bonus">
 								</label>
 							</div>
 						</div>
 					</div>
+
+					<div class="order-done--bonus congratulations">
+						<h6>Поздравляем</h6>
+						<div class="text-center">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita aliquam, voluptatibus placeat dolores maiores magni eveniet odit quaerat molestias, omnis, temporibus voluptatum. Culpa ratione, repellendus minus, accusamus excepturi laborum sint?</p>
+						</div>
+						<div class="bonus-selector flex align-center">
+							<div class="choise column unchosen">
+								<label class="choise-block">
+									<div class="choise-gift">
+										<span class="icon" style="background-image: url(public/img/icons/pocketico.png)"></span>
+										<span class="name">Промокод на скидку</span>
+									</div>
+									<input type="radio" name="bonus">
+								</label>
+							</div>
+							<div class="choise column unchosen">
+								<label class="choise-block">
+									<div class="choise-gift">
+										<span class="icon" style="background-image: url(public/img/icons/pacmanico.png)"></span>
+										<span class="name">скидка на аккаунт</span>
+									</div>
+									<input type="radio" name="bonus">
+								</label>
+							</div>
+							<div class="choise column red opened chosen">
+								<label class="choise-block">
+									<div class="choise-gift">
+										<span class="icon" style="background-image: url(public/img/icons/discountico.png)"></span>
+										<span class="name">-15% на следующий буст</span>
+									</div>
+									<input type="radio" name="bonus">
+								</label>
+							</div>
+						</div>
+						<div class="text-center order-done--footer">
+							<label class="promo-code-line">
+								<span>Ваш промокод на скидку: <input type="text" value="914JDewna" readonly disabled></span>
+								<button class="runner">Скопировать</button>
+							</label>
+							<p class="notification">Код действует в течении 30 дней.</p>							
+						</div>
+					</div>
+
 				</div>
 			</div>				
 
